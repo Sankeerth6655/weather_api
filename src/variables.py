72c90b25de97b1738cef18c0a260354aa221eb65
AksharaDondeti <aksharadondeti@gmail.com>
2024-09-20 08:37:08 +0200

    initial_setup

---
+++ src/variables.py
@@ -0,0 +1,11 @@
+CONFIG={
+    "database": {
+      "xyz": {
+        "user": "vjit",
+        "password": "Str0ngP@ssw0rd!",
+        "host": "161.97.186.196",
+        "database": "weatheri",
+        "port": 3306
+      }
+    }
+}
